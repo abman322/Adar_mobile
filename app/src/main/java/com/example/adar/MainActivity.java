c0f6a47b96cb3709b263c11431b1ad6afa3c3dbf
liulalemx <liulalemx@gmail.com>
2023-02-07 12:37:09 +0300

    Created Login page

--- app/src/main/java/com/example/adar/MainActivity.java
+++ app/src/main/java/com/example/adar/MainActivity.java
@@ -10,33 +10,13 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        if(mAuth.getCurrentUser() == null) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-            return;
-        }
-
-        Button signOut = findViewById(R.id.signOutBtn);
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logoutUser();
-            }
-        });
     }
 
-    private void logoutUser(){
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
 }
