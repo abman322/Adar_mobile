0d7d43c3b0761eb8f9cc9881265551e0bc2fca0a
liulalemx <liulalemx@gmail.com>
2023-02-08 16:51:44 +0300

    Implemented firebase auth password recovery

--- app/src/main/java/com/example/adar/ForgotPassword.java
+++ app/src/main/java/com/example/adar/ForgotPassword.java
@@ -1,5 +1,6 @@
 package com.example.adar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPassword extends AppCompatActivity {
 
     private EditText mRecoveryEmail;
     private Button mRecoveryEmailButton;
     private TextView mGotoLogin;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class ForgotPassword extends AppCompatActivity {
         mRecoveryEmail = findViewById(R.id.recoveryEmail);
         mRecoveryEmailButton = findViewById(R.id.recoveryEmailButton);
         mGotoLogin = findViewById(R.id.gotoLogin);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mGotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +51,18 @@ public class ForgotPassword extends AppCompatActivity {
                 if(email.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_SHORT).show();
                 }else{
-                    // Send recovery email
+                    mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Check your email to recover password", Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(ForgotPassword.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Unable to send recovery email", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
