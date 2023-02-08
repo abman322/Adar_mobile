2c874830040b4ea6df45e7a942af717827339a5b
liulalemx <liulalemx@gmail.com>
2023-02-08 19:30:57 +0300

    Added Bottom Navigation and Styled Home Fragment

--- app/src/main/java/com/example/adar/HomePage.java
+++ app/src/main/java/com/example/adar/HomePage.java
@@ -1,14 +1,54 @@
 package com.example.adar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomePage extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    MyHomesFragment myHomesFragment = new MyHomesFragment();
+    RentedHomesFragment rentedHomesFragment = new RentedHomesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        getSupportActionBar().hide(); // Hides the action bar
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+               switch (item.getItemId()){
+                   case R.id.home:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                       return true;
+                   case R.id.rentedHomes:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.container, rentedHomesFragment).commit();
+                       return true;
+                   case R.id.yourHomes:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.container, myHomesFragment).commit();
+                       return true;
+                   case R.id.profile:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                       return true;
+
+               }
+               return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/adar/MyHomesFragment.java
@@ -0,0 +1,18 @@
+package com.example.adar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyHomesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_homes, container, false);
+    }
+}
