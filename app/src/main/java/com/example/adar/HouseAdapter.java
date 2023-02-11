e7ec7a9e62a0f582d4852ad6227ef9f670ff9abf
liulalemx <liulalemx@gmail.com>
2023-02-11 11:12:00 +0300

    Created House Recycler view layout and Data class

---
+++ app/src/main/java/com/example/adar/HouseAdapter.java
@@ -0,0 +1,4 @@
+package com.example.adar;
+
+public class HouseAdapter {
+}
---
+++ app/src/main/java/com/example/adar/HouseData.java
@@ -0,0 +1,19 @@
+package com.example.adar;
+
+public class HouseData {
+
+    private String city, country, price, imageURL;
+
+    public String getCity(){
+        return city;
+    }
+    public String getCountry(){
+        return country;
+    }
+    public String getPrice(){
+        return price;
+    }
+    public String getImageURL(){
+        return imageURL;
+    }
+}
